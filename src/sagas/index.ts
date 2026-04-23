e4caa90c441401c762e6e681c4ed1dbbbc4e012c
reshalmenzes <reshal.menezes@xaviers.edu.in>
2026-04-23 14:13:08 +0530

    Update index.ts

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects';
+import { SagaIterator } from 'redux-saga';
 import { watchEmail } from './email';
 import {watchAuth} from './auth';
 
-export default function* rootSaga()  {
+export default function* rootSaga(): SagaIterator {
     return yield all([watchEmail(), watchAuth()]);
 }
